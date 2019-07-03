07410447e0aba53e4e9eb192b6490ebe957ddd0f
Matt Gatner <mgatner@tattersoftware.com>
2019-07-03 14:17:45 +0000

    Update default session user

--- bin/Audits.php
+++ bin/Audits.php
@@ -15,7 +15,7 @@ use CodeIgniter\Config\BaseConfig;
 class Audits extends \Tatter\Audits\Config\Audits
 {
 	// key in $_SESSION that contains the integer ID of a logged in user
-	public $sessionUserId = "userId";
+	public $sessionUserId = "logged_in";
 
 	// whether to continue instead of throwing exceptions
 	public $silent = true;
--- src/Config/Audits.php
+++ src/Config/Audits.php
@@ -5,7 +5,7 @@ use CodeIgniter\Config\BaseConfig;
 class Audits extends BaseConfig
 {
 	// key in $_SESSION that contains the integer ID of a logged in user
-	public $sessionUserId = "userId";
+	public $sessionUserId = "logged_in";
 
 	// whether to continue instead of throwing exceptions
 	public $silent = true;
